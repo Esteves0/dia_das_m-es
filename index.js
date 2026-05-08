444ca05bac73bed6869f25ac859a68aef926c89e
Esteves0 <guilherme.esteves6@aluno.senai.br>
2026-05-08 16:37:00 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -18,10 +18,13 @@ const listaImagens = [
     "fotos_mae/foto_8.jpeg"
 
 ]
+const myAudio = new Audio('musica.mp3');
 const frase = document.getElementById('frase')
 const imagem = document.getElementById('imagem')
 let indiceAtual = 0
 let ultimoIndice = listaImagens.length - 1
+
+
 
 frase.textContent = listaFrases[indiceAtual]
 imagem.src = listaImagens[indiceAtual]
@@ -29,18 +32,27 @@ imagem.src = listaImagens[indiceAtual]
 
 
 function proximaFoto(){
-
-    if (indiceAtual < ultimoIndice){
-        //Podeir para próximo
-        indiceAtual++
-    }
-    else if(indiceAtual == ultimoIndice){
-        indiceAtual = 0
-    }
-
-    frase.textContent = listaFrases[indiceAtual]
-    imagem.src = listaImagens[indiceAtual]
+    // Toca a música exatamente como você colocou
+    myAudio.play();
     
 
-    
+    // Adiciona o efeito de sumir
+    imagem.classList.add("fade-out");
+
+    // Espera o efeito de sumir para trocar os dados
+    setTimeout(() => {
+        // SUA LÓGICA ORIGINAL EXATA
+        if (indiceAtual < ultimoIndice){
+            indiceAtual++
+        }
+        else if(indiceAtual == ultimoIndice){
+            indiceAtual = 0
+        }
+
+        frase.textContent = listaFrases[indiceAtual]
+        imagem.src = listaImagens[indiceAtual]
+
+        // Faz a imagem aparecer de novo
+        imagem.classList.remove("fade-out");
+    }, 500); // 500ms é o tempo da transição
 }
